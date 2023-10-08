e21c6ac786c53693c349d1dd66adadfe3333d950
Egor Gusev <guy.do.or.die@gmail.com>
2023-10-08 17:52:49 +0400

    PoC contracts

--- contracts/src/AHandBase.sol
+++ contracts/src/AHandBase.sol
@@ -2,7 +2,17 @@
 
 pragma solidity >=0.8.20;
 
+import "./AHand.sol";
 
-contract AHand {
+
+contract AHandBase {
+
+    mapping(uint => address) public hands;
+    uint public handsNumber;
+
+    function raise(string calldata problem) public payable {
+        AHand aHand = new AHand{value: msg.value}(msg.sender, problem);
+        hands[handsNumber++] = address(aHand);
+    }
 
 }
